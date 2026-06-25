ae3d4fd0fdea581b39f5f968458dbe979ce86e18
Syed Athar <157048627+SyedAthar88@users.noreply.github.com>
2026-06-26 02:08:44 +0500

    Time: 1 ms (88.07%), Space: 58.2 MB (27.83%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let count =0;
+    let candidate=null;
+    for(let num of nums){
+        if(count===0){
+            candidate=num;
+            count =1;
+        }
+        else if(num===candidate){
+            count ++;
+        }else{
+            count --;
+        }
+    }
+    return candidate;
+};
