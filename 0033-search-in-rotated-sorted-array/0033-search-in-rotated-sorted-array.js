f1f84d38a1ff979f399526cd0e2bef7c0e48cf18
Syed Athar <157048627+SyedAthar88@users.noreply.github.com>
2026-06-28 06:56:43 +0500

    Time: 0 ms (100%), Space: 52.5 MB (97.96%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let st = 0;
+    let end = nums.length - 1;
+    while (st <= end) {
+        let mid = st + Math.floor((end - st) / 2)
+        if (nums[mid] === target) {
+            return mid;
+        }
+        if (nums[st] <= nums[mid]) { //left sorted array
+            if (nums[st] <= target && target <= nums[mid]) {
+                end = mid - 1;
+            }else{
+                st=mid+1;
+            }
+        }
+        else {
+            if (nums[mid] <= target && target <= nums[end]) {
+                st = mid + 1;
+            }else{
+                end=mid-1
+            }
+        }
+
+    }
+    return -1;
+};
