7bbf238cdda9bd4edca8acd382cdf34876766272
Syed Athar <157048627+SyedAthar88@users.noreply.github.com>
2026-07-27 20:58:13 +0500

    Time: 0 ms (100%), Space: 53.8 MB (34.08%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    const result = [];
+                if (matrix.length === 0) return result;
+                
+                let top = 0;
+                let bottom = matrix.length - 1;
+                let left = 0;
+                let right = matrix[0].length - 1;
+                
+                while (top <= bottom && left <= right) {
+                // Traverse from left to right on the top row
+                for (let i = left; i <= right; i++) {
+                result.push(matrix[top][i]);
+                }
+                top++;
+                
+                // Traverse from top to bottom on the right column
+                for (let i = top; i <= bottom; i++) {
+                result.push(matrix[i][right]);
+                }
+                right--;
+                
+                // Traverse from right to left on the bottom row (if still valid)
+                if (top <= bottom) {
+                for (let i = right; i >= left; i--) {
+                result.push(matrix[bottom][i]);
+                }
+                bottom--;
+                }
+                
+                // Traverse from bottom to top on the left column (if still valid)
+                if (left <= right) {
+                for (let i = bottom; i >= top; i--) {
+                result.push(matrix[i][left]);
+                }
+                left++;
+                }
+                }
+                
+                return result;
+};
