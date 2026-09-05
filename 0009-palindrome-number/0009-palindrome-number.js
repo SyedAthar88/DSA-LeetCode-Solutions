f4018deed4eeee4b690ccac0fb817d7ac532a299
Syed Athar <157048627+SyedAthar88@users.noreply.github.com>
2026-09-05 15:58:14 +0500

    Time: 5 ms (77.67%), Space: 62.3 MB (96.54%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -7,7 +7,7 @@ var isPalindrome = function (x) {
     let str = x.toString()
     let left = 0;
     let right = str.length - 1;
-    while (left <= right) {
+    while (left < right) {
         if (str[left] !== str[right]) return false;
         left++;
         right--;
