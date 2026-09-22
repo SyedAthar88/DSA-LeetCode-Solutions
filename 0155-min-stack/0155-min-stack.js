e3d6f0fa21648e7ee8907905578856830a0be7d0
Syed Athar <157048627+SyedAthar88@users.noreply.github.com>
2026-09-23 03:50:18 +0500

    Time: 102 ms (49.65%), Space: 88.7 MB (16.12%) - LeetHub

--- 0155-min-stack/0155-min-stack.js
+++ 0155-min-stack/0155-min-stack.js
@@ -1,23 +1,18 @@
-/**
- * initialize your data structure here.
- */
+
 var MinStack = function () {
     this.stack = [];
     this.minStack = [];
 };
 
 /** 
- * @param {number} val
+ * @param {number} value
  * @return {void}
  */
-MinStack.prototype.push = function (val) {
-    this.stack.push(val);
-
-    // only add to minStack if it's the first element, or val is a new minimum (or tie)
-    if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
-        this.minStack.push(val);
+MinStack.prototype.push = function (value) {
+    this.stack.push(value);
+    if (this.minStack.length === 0 || value <= this.minStack[this.minStack.length - 1]) {
+        this.minStack.push(value)
     }
-    // else: do nothing, minStack stays as it is
 };
 
 /**
@@ -25,10 +20,8 @@ MinStack.prototype.push = function (val) {
  */
 MinStack.prototype.pop = function () {
     const removed = this.stack.pop();
-
-    // only remove from minStack if the value we just removed was the current minimum
     if (removed === this.minStack[this.minStack.length - 1]) {
-        this.minStack.pop();
+        this.minStack.pop()
     }
 };
 
@@ -43,13 +36,13 @@ MinStack.prototype.top = function () {
  * @return {number}
  */
 MinStack.prototype.getMin = function () {
-    return this.minStack[this.minStack.length - 1];
+    return this.minStack[this.minStack.length - 1]
 };
 
 /** 
  * Your MinStack object will be instantiated and called as such:
  * var obj = new MinStack()
- * obj.push(val)
+ * obj.push(value)
  * obj.pop()
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
